1677c14f5861c4cb6d5fc52077a992b556a5992e
pkkoshta <prashant30koshta@gmail.com>
2023-07-19 14:35:18 +0530

    backend project

---
+++ src/main/java/com/jai/SpringBootAngularMaterialAppApplication.java
@@ -0,0 +1,13 @@
+package com.jai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularMaterialAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularMaterialAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jai/SpringBootAngularMaterialAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAngularMaterialAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
